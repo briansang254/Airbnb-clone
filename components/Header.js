41aa76ff08c677f10ef9f95aa462cd3790b1f1f3
BRIAN SANG <briantheboss442@gmail.com>
2023-02-09 17:18:29 +0300

    first commit

---
+++ components/Header.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import Image from 'next/image'
+import {
+  MagnifyingGlassIcon,
+  UserCircleIcon,
+  UsersIcon,
+  GlobeAltIcon,
+  Bars4Icon
+  
+} from '@heroicons/react/24/outline'
+function Header() {
+  return (
+    <header className='sticky top-0 z-50 grid grid-cols-3 bg-white
+    shadow-md p-5 '>
+        <div className='relative flex h-10 cursor-pointer my-auto'>
+            <Image
+                src="https://links.papareact.com/qd3"
+                layout='fill'
+                objectFit='contain'
+                objectPosition='left'
+                alt='logo'
+                />
+
+        </div>
+
+        {/* Search section */}
+        <div className='flex items-center border-2 rounded-full
+         py-2 shadow'>
+          <input type={'text'} 
+          placeholder='Start your search'
+          className='pl-5 bg-transparent outline-none flex-grow text-sm
+          text-gray-600 placeholder-gray-400'
+          />
+          <MagnifyingGlassIcon
+           className=' hidden md:inline-flex h-10 text-white bg-red-400
+            rounded-full p-2 cursor-pointer md:mx-2' />
+        </div>
+
+        {/* Right section */}
+        <div  className='flex items-center justify-end space-x-4 text-gray-500 '>
+          <p className='hidden md:inline cursor-pointer'>Become a host</p>
+          <GlobeAltIcon  
+          className='h-6 cursor-pointer'
+          />
+          <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
+            <Bars4Icon className='h-6'/>
+            <UserCircleIcon className='h-6'/>
+          </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ next.config.js
@@ -0,0 +1,15 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ["links.papareact.com"],
+//   },
+// }
+
+// module.exports = nextConfig
+
+module.exports = {
+  images: {
+    domains: ["links.papareact.com"],
+  },
+};
